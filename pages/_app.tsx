cb4edf3768e6715e56248e69a069e4659c357acf
Marllon Gomes <marllon.fgomes@gmail.com>
2021-07-04 01:53:54 -0300

    commit inicial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,7 @@
 import type { AppProps } from 'next/app'
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    font-family: 'Source Code Pro', monospace;
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    list-style: none;
-    text-decoration: none;
-  }
-`;
+import '../styles/app.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 export default MyApp
